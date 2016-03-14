c12caf948b63ab4a3fa6e54a3e72ca284b28342a
abluciano <abluciano@mail.ru>
2016-03-14 12:42:45 +0700

    Added files via upload

--- Hwork3/src/pack/Run.java
+++ Hwork3/src/pack/Run.java
@@ -4,10 +4,11 @@ public class Run {
 
 	public static void main(String[] args) {
 		
-		Matrix M = new Matrix(5, 5);
-		int[][] D = {{1,2},{3,4}};
-	//	M.fillMatrix();
-	//	M.sum(D);
+		Matrix M = new Matrix(2, 2);
+		Matrix D = new Matrix (2, 2);
+		D.fillMatrix();
+		M.fillMatrix();
+		M.sum(D);
 	//	M.dim(D);
 	//	M.invers(D);
 	//	M.mult(M.ArrMatrix, 3);

1b1f2e88ad9ccc5f79da9d182ceeeedab01bb9fd
abluciano <abluciano@mail.ru>
2016-03-24 16:25:28 +0600

    Added files via upload

--- Collect/src/pack/Run.java
+++ Collect/src/pack/Run.java
@@ -1,17 +1,21 @@
 package pack;
 
-import java.awt.List;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
 public class Run {
 
 	public static void main(String[] args) {
-		
-		ArrayList div = new ArrayList();
-	//	HashSet div = new HashSet();
-		
+
+		ArrayList<Employee> div = new ArrayList<Employee>();
+		Set<Integer> set1 = new LinkedHashSet<>();
+		Set<Integer> set2 = new LinkedHashSet<>();
+
 		div.add(new Employee("Иванов И.И.", 12));
 		div.add(new Employee("Перов П.П,", 1));
 		div.add(new Employee("Сидоров С.С.", 8));
@@ -20,27 +24,26 @@ public class Run {
 		div.add(new Employee("Хоттабыч Г.А.", 312));
 		div.add(new Employee("Николаев В.С.", 5));
 		div.add(new Employee("Гермиона Г.", 9));
-		
-		prnList(div, 10);
 
-	}
-	
-	public static void prnList(ArrayList A, int j){
-		
-		Iterator iter = A.iterator();
-		
-		Employee elem;
-		int i=0;
-		
-		System.out.println("Больше "+j+" лет на нас отработали:\n");
-		while (iter.hasNext()) {
-			elem = (Employee)A.get(i); 
-			if (elem.workAge >= j){
-				System.out.println(elem.fio);
-			}
-			i++;
-		}
-		
+		HashSet<Employee> div1 = new HashSet<Employee>(div);
+		/*
+		 * Queue<Employee> div2 = new PriorityQueue<>();
+		 * 
+		 * for(int i=0; i <= div.size(); i++ ){ div2.add((Employee) div.get(i));
+		 * }
+		 * 
+		 */
+
+		WorkSet.fillSet(set1, 5, 12);
+		WorkSet.fillSet(set2, 5, 10);
+		System.out.println(set1);
+		System.out.println(set2);
+		WorkSet.union(set1, set2);
+		WorkSet.intersect(set1,set2);
+
+		WorkSet.prnList(div, 10);
+		WorkSet.prnList(div1, 10);
+
 	}
 
 }
---
+++ Collect/src/pack/WorkSet.java
@@ -0,0 +1,93 @@
+package pack;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public abstract class WorkSet {
+
+	public static void fillSet(Set A, int len, int j) {
+
+		for (int i = 0; i < len; i++) {
+			A.add(i + j);
+		}
+
+	}
+
+	public static void prnList(HashSet A, int j) {
+
+		Iterator iter = A.iterator();
+
+		Employee elem;
+
+		System.out.println("Больше " + j + " лет на нас отработали:\n");
+		while (iter.hasNext()) {
+			elem = (Employee) iter.next();
+			if (elem.workAge >= j) {
+				System.out.println(elem.fio);
+			}
+
+		}
+
+	}
+	
+	public static void prnList(ArrayList A, int j) {
+
+		Iterator iter = A.iterator();
+
+		Employee elem;
+
+		System.out.println("Больше " + j + " лет на нас отработали:\n");
+		while (iter.hasNext()) {
+			elem = (Employee) iter.next();
+			if (elem.workAge >= j) {
+				System.out.println(elem.fio);
+			}
+
+		}
+
+	}
+	
+	public static void union(Set A, Set B){
+		ArrayList<Integer> res = new ArrayList<>();
+		Iterator itA = A.iterator();
+		Iterator itB = B.iterator();
+		while(itA.hasNext()){
+			res.add((Integer) itA.next());
+		}
+		
+		while(itB.hasNext()){
+			res.add((Integer) itB.next());
+		}
+		
+		System.out.println("Объединение множеств: "+res);
+		
+	}
+	
+	public static void intersect(Set A, Set B){
+		
+		ArrayList<Integer> res = new ArrayList<>();
+		ArrayList<Integer> forB = new ArrayList<>();
+		Iterator itA = A.iterator();
+		Iterator itB = B.iterator();
+		int a,b;
+		
+		while(itB.hasNext()){
+			forB.add((Integer) itB.next());
+		}
+		
+		while(itA.hasNext()){
+			a = (int)itA.next();
+			for(int i=0; i<forB.size();i++){
+				b = forB.get(i);
+				if(a == b){
+					res.add(a);
+				}
+			}
+		}
+		
+		System.out.println("Пересечение множеств: "+res);
+	}
+	
+}

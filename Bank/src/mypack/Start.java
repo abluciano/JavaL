e3268ad54db41552075a38938a4facff33cfc8a1
abluciano <abluciano@mail.ru>
2016-04-05 12:12:50 +0600

    Added files via upload

--- Bank/src/mypack/Start.java
+++ Bank/src/mypack/Start.java
@@ -8,12 +8,18 @@ public class Start {
 
 		ModifyAccount salary = new ModifyAccount(sberbank);
 		ModifyAccount autopay = new ModifyAccount(sberbank, 1000);
+//		ModifyAccount autopay2 = new ModifyAccount(sberbank, 1000);
+//		ModifyAccount autopay3 = new ModifyAccount(sberbank, 1000);
+//		ModifyAccount autopay4 = new ModifyAccount(sberbank, 1000);
 
 		salary.start();
 		autopay.start();
+//		autopay2.start();
+//		autopay3.start();
+//		autopay4.start();
 
 		while (true) {
-			if (!salary.isAlive() && !autopay.isAlive()) {
+			if (!salary.isAlive() && !autopay.isAlive() /*&& !autopay2.isAlive()&& !autopay3.isAlive()&& !autopay4.isAlive()*/) {
 				System.out.println("Остаток на Вашем счету: " + sberbank.getRestSum() + " руб.");
 				break;
 			}
